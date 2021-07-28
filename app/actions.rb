6b0ee0db3fc0876145679d6d5879479a9b73d36d
Pavan <pavan.thind@gmail.com>
2021-07-28 23:56:10 +0000

    W5D2 half

--- app/actions.rb
+++ app/actions.rb
@@ -4,6 +4,22 @@ helpers do
     end
 end
 
+get '/finstagram_posts/new' do
+    @finstagram_post = FinstagramPost.new
+    erb(:"finstagram_posts/new")
+end
+
+post '/finstagram_posts' do
+   photo_url = params[:photo_url]
+
+   @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id})
+
+   if @finstagram_post.save 
+    redirect(to('/'))
+   else
+    erb(:"finstagram_posts/new")
+   end
+end
 
 get '/login' do
     erb(:login)
